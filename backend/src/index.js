ead79e4b5ef0defae45b782d624698a8b9dc418b
rubberducky94 <michaelsnair1981@gmail.com>
2025-09-13 20:57:12 +0000

    Revert "render compatibility agian"
    This reverts commit 5f70650581827a4c8dfa0ca6434aee215afcdf19.

--- backend/src/index.js
+++ backend/src/index.js
@@ -144,5 +144,11 @@ app.get('/api/entries', authMiddleware, async (req, res) => {
   }
 });
 
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on ${port}`));
